273a8a550f74c15af40d161c1c242d8e04cf19d1
kendall willard <kendallwillard@hotmail.com>
2019-01-27 21:48:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
+
+function appendKitten(name) {
+  var result = kittens.push(name)
+  return result
+}
